9a505591c2c6fd86b826b100515ff1ff4b5008e4
Greg Mackelden <gmackelden@gmail.com>
2014-10-29 16:18:12 +0100

    incremental update, this currently does word probability for the whole text

--- main.py
+++ main.py
@@ -13,40 +13,59 @@ def get_sentence_tokens(text):
 	sent_tokens = sent_tokenize(text)
 	return sent_tokens
 	
-def get_word_tokens(text):
-	# tokenise the text, removing punctuation and stopwords
-	word_tokens = word_tokenize(sub('[^\w\s]', '', text))
-	word_tokens = [w for w in word_tokens if w not in stop_words]
-
+def get_word_frequency(word_tokens):
 	# get the most common words
 	# TODO: amount depending on length of text
 	frequency = FreqDist(word_tokens)
-	most_common = frequency.most_common(10)
+	most_common = frequency.most_common(len(word_tokens))
+
+	length = sum([w[1] for w in most_common])
+	tokens = {w[0]: {'freq':w[1], 'prob':w[1]/length} for w in most_common}
 
 	# return the words without their counts
-	return [w[0] for w in most_common]
-	
+	return tokens
+
+def get_word_tokens(text, get_most_frequent=True):
+	# tokenise the text, removing punctuation and stopwords
+	word_tokens = word_tokenize(strip_text(text))
+	word_tokens = [w for w in word_tokens if w not in stop_words]
+	return word_tokens
+
+def strip_text(text):
+	return sub('[^a-zA-Z\s\-]', '', text.lower())
 
 def run(text):
-	text_lower = text.lower()
-	sent_tokens = get_sentence_tokens(text)
-	word_tokens = get_word_tokens(text)
+	text = text.lower()
+	sentences = get_sentence_tokens(text)
+	text_tokens = get_word_tokens(text)
+	text_frequency = get_word_frequency(text_tokens)
+	weighted = []
 
-	# generate a list of the sentences with a count of how many word tokens appear in them
-	summ_sentences = [[sentence, 0] for sentence in sent_tokens]
-	for index in range(0, len(summ_sentences)):
-		sentence = summ_sentences[index]
-		for word in word_tokens:
-			if word in sentence[0]:
-				sentence[1] += 1
+	for index, sentence in enumerate(sentences):
+		sentence_tokens = get_word_tokens(sentence)
+		sentence_frequency = get_word_frequency(sentence_tokens)
+		score = None
+		for word in sentence_tokens:
+			word_score = sentence_frequency[word].get('prop', 0)
+			word_score += text_frequency[word].get('prob', 0)
+			score = word_score if score==None else(score + word_score) / 2
+		weighted.append((sentence, score, index))
 
-	# generate the summary text by dropping non common sentences
-	common = [s[0] for s in summ_sentences if s[1]]
-	summary = " ".join(common)
+	from math import floor
+	divisor = 3
+	# sort weighted by sentence probability
+	weighted.sort(key=lambda tup: tup[1])
+	cut_off = list(set([s[1] for s in weighted]))
+	cut_off = cut_off[floor(len(cut_off)/divisor)]
+	print('cut off', cut_off)
 
+	# sort weighted by sentence index
+	weighted.sort(key=lambda tup: tup[2])
+	selected = [s[0] for s in weighted if s[1] >= cut_off]
+	summary = ' '.join([s.capitalize() for s in selected])
 	print(summary)
 	print()
-	print('summarised', len(text), 'words to', len(summary), 'words')
+	print('summarised', len(sentences), 'sentences to', len(selected), 'sentences')
 
 if __name__ == '__main__':
 	# run the summarise method with the sample text file if it exists
